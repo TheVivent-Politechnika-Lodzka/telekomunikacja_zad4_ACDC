d42a70da19bca60e1cbbc58c379d5ef49376c2b4
Adam Vivent Kapuściński <a.kapuscinski@thevivent.com>
2021-05-19 13:51:24 +0200

    Co-authored-by: Damian Szczeciński <230016@edu.p.lodz.pl>

--- kod/main.py
+++ kod/main.py
@@ -2,18 +2,45 @@ from ac import Recorder
 from ca import Player
 import keyboard as kb
 
-
-test = Recorder()
-
-while not kb.is_pressed("q"):
-    if kb.is_pressed("r"):
-        test.record(1)
+try:
+    samplerate = abs(int(input("Podaj częstotliwość próbkowania [48000]: ")))
+except:
+    samplerate = 48000
 
 
+try:
+    quantization = int(input("Wybierz poziom kwantyzacji (8, [16], 32): "))
+except:
+    quantization = 16
+if quantization not in [8, 16, 32]:
+    exit("Czytac nie umiesz?")
 
-test.saveToFile("test8", 8)
-test.saveToFile("test16", 16)
-test.saveToFile("test32", 32)
 
-testAudio = Player()
-testAudio.playData(test.getAudio())
+REC = Recorder(samplerate=samplerate)
+
+print("""
+    Trzymaj 'r' aby nagrywać
+    Wciśnij 's' aby zapisać nagranie
+    Wciśnij 'p' aby odtworzyć nagranie
+    Wciśnij 'q' aby zakończyć program
+""")
+
+PLAY = Player(samplerate=samplerate)
+last_filename = ""
+
+while not kb.is_pressed('q'):
+    if kb.is_pressed('r'):
+        REC.record(1)
+    if kb.is_pressed('s'):
+        last_filename = input("Nazwij plik (nie podawaj rozszerzenia): ")
+        REC.saveToFile(last_filename, quantization)
+    if kb.is_pressed('p'):
+        print("Odtwarzam oryginalne nagranie...")
+        SNR = PLAY.playData(REC.getAudio())
+        print("SNR oryginalnego nagrania to: {}dB".format(SNR))
+        if last_filename != "":
+            print("Odtwarzam zapisane nagranie...")
+            SNR = PLAY.playWav("{}.wav".format(last_filename))
+            print("SNR zapisanego nagrania to: {}dB".format(SNR))
+
+
